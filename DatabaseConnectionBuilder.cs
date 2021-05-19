f3d7f481e8cfbfced947b8d344bb9dadf8e09867
lia-ghita <30419671+lia-ghita@users.noreply.github.com>
2021-05-19 21:58:32 +0300

    initial commit

---
+++ DatabaseConnectionBuilder.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Windows.UI.Popups;
+
+namespace MikypediaUWP
+{
+    
+        class DatabaseConnectionBuilder
+        {
+            private DbTypes type = DbTypes.Undefined;
+            private string host = "localhost";
+            private string username = "root";
+            private string password = "";
+            private string dbName;
+            public DatabaseConnectionBuilder withType(DbTypes type)
+            {
+                this.type = type;
+                return this;
+            }
+
+
+            public DatabaseConnectionBuilder setHost(string hostname)
+            {
+
+                this.host = hostname;
+                return this;
+            }
+
+            public DatabaseConnectionBuilder setUsername(string username)
+            {
+
+                this.username = username;
+                return this;
+            }
+
+            public DatabaseConnectionBuilder setPassword(string password)
+            {
+
+                this.password = password;
+                return this;
+            }
+
+            public DatabaseConnectionBuilder setdbName(string dbName)
+            {
+
+                this.dbName = dbName;
+                return this;
+            }
+
+            public DbConnection build()
+            {
+                if (type == DbTypes.Undefined)
+                {
+                    throw new Exception("Missing database type");
+                }
+
+                if (type == DbTypes.MySQL)
+                {
+                    return buildMySQLConnection();
+                }
+                else if (type == DbTypes.MSSQL)
+                {
+                    return buildMSSQLConnection();
+                }
+                else
+                {
+                    throw new Exception("Unsupported database type " + type);
+                }
+            }
+
+            private DbConnection buildMySQLConnection()
+            {
+
+                MySqlConnection conn = new MySqlConnection();
+
+                if (host == null)
+                {
+                    throw new Exception("missing hostname");
+                }
+                try
+                {
+                    string connectionString = "server=" + this.host + "; database=" + this.dbName + " ;userid=" + this.username + " ;password=" + this.password;
+                    conn.ConnectionString = connectionString;
+                    conn.Open();
+                }
+
+                catch (MySql.Data.MySqlClient.MySqlException ex)
+                {
+                var dialog = new MessageDialog(ex.Message);
+                
+            }
+                return conn;
+            }
+
+            private DbConnection buildMSSQLConnection()
+            {
+                SqlConnection cnn = null;
+                if (host == null)
+                {
+                    throw new Exception("missing hostname");
+                }
+                try
+                {
+                    string connectionString;
+                    connectionString = @"Data Source=" + this.host + "; Initial Catalog=" + this.dbName + "; Integrated Security = True";
+                    cnn = new SqlConnection(connectionString);
+                    cnn.Open();
+                }
+                catch (SqlException ex)
+                {
+                var dialog = new MessageDialog(ex.Message);
+            }
+
+                return cnn;
+            }
+
+        }
+
+
+        enum DbTypes
+        {
+
+            Undefined = -1, MySQL = 1, MSSQL = 2
+        }
+
+
+    }
+
+
+
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -12,6 +12,13 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Windows.UI.Popups;
+using System.Data.Common;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +32,60 @@ namespace MikypediaUWP
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void FlipView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Enum.TryParse(DBType.Text, out DbTypes type);
+
+            try
+            {
+
+                DbConnection dbConn = new DatabaseConnectionBuilder()
+                    .withType(type)
+                    .setHost(host.Text)
+                    .setdbName(name.Text)
+                    .setUsername(username.Text)
+                    .setPassword(password.Text)
+                    .build();
+
+
+                this.Hide();
+                Editor = new MikyPediaSQLClient(dbConn);
+                Editor.Show();
+            }
+            catch (Exception exception)
+            {
+                var dialog = new MessageDialog("Error when attempting to open the connection " + exception);
+                
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (DBType.Text == "MSSQL")
+            {
+                host.Text = "localhost";
+                name.Text = "lab1";
+                WAuth.IsChecked = true;
+            }
+            else if (DBType.Text == "MySQL")
+            {
+                WAuth.IsChecked = false;
+                host.Text = "";
+                name.Text = "";
+
+            }
         }
     }
 }
